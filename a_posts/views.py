7d936edbcc505aa050b7528c0b9a50a7d4d000ba
BenjaminMehrez <benjaminjofre01@gmail.com>
2024-09-06 17:04:11 -0300

    update load page cap19

--- a_posts/views.py
+++ a_posts/views.py
@@ -7,6 +7,7 @@ from .forms import *
 from bs4 import BeautifulSoup
 import requests
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -19,10 +20,25 @@ def home_view(request, tag=None):
     else:
         posts = Post.objects.all()
         
+        
+    paginator = Paginator(posts, 3)
+    page = int(request.GET.get('page', 1))
+    
+    try:
+        posts = paginator.page(page)
+    except:
+        return HttpResponse('')
+    
+       
     context = {
         'posts': posts,
-        'tag': tag
+        'tag': tag,
+        'page' : page,
     }
+    
+    
+    if request.htmx:
+        return render(request, 'snippets/loop_home_posts.html', context)
 
     return render(request, 'a_posts/home.html', context)
 
