8c3f125d8ba7bc5b2913cb62112d03ad3aaaa0d1
BenjaminMehrez <benjaminjofre01@gmail.com>
2024-09-16 22:50:16 -0300

    updateseo3

--- a_posts/sitemaps.py
+++ a_posts/sitemaps.py
@@ -1,21 +1,21 @@
-from django.contrib.sitemaps import Sitemap
-from django.urls import reverse
-from .models import Tag, Post
+# from django.contrib.sitemaps import Sitemap
+# from django.urls import reverse
+# from .models import Tag, Post
 
 
-class StaticSitemap(Sitemap):
-    def items(self):
-        return ['home']
+# class StaticSitemap(Sitemap):
+#     def items(self):
+#         return ['home']
 
-    def location(self, item):
-        return reverse(item)
-
-
-class CategorySitemap(Sitemap):
-    def items(self):
-        return Tag.objects.all()
+#     def location(self, item):
+#         return reverse(item)
 
 
-class PostpageSitemap(Sitemap):
-    def items(self):
-        return Post.objects.all()[:100]
+# class CategorySitemap(Sitemap):
+#     def items(self):
+#         return Tag.objects.all()
+
+
+# class PostpageSitemap(Sitemap):
+#     def items(self):
+#         return Post.objects.all()[:100]
